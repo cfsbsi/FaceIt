87e22d099e74f4c2a32f5094a8cc7b8ade45b0e1
Christian Franco Soares <christian@Christians-MacBook-Pro.local>
2017-04-29 22:17:04 -0300

    Initial Commit

---
+++ FaceIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by Christian Franco Soares on 29/04/17.
+//  Copyright © 2017 Pandunia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
